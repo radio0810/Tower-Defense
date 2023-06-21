7e70a96035a7324b903960b8d4fed5decb50b72f
radio0810 <radio0810@gmail.com>
2023-06-21 11:59:23 -0400

    enemy movement
    smoothed enemy movement along a path

--- Tower Defense/Assets/Scripts/Waypoint.cs
+++ Tower Defense/Assets/Scripts/Waypoint.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] bool isPlaceable;
+    [SerializeField] GameObject towerPrefab;
+    void OnMouseDown()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(isPlaceable)
+        {
+            
+            Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            isPlaceable = false;
+            
+        }
+        else
+        {
+            Debug.Log("Can't place here");
+        }
+       
     }
 }
