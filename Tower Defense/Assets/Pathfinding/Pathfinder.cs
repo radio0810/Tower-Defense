7ba70167eaf81094d7f115fd2045234c3c437a61
radio0810 <radio0810@gmail.com>
2023-06-26 21:52:14 -0400

    pathfinding
    worked on breadth first pathfiding

---
+++ Tower Defense/Assets/Pathfinding/Pathfinder.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    [SerializeField] Vector2Int startCoordinates;
+    [SerializeField] Vector2Int destinationCoordinates;
+
+    Queue<Node> frontier = new Queue<Node>();
+    Dictionary<Vector2Int, Node> reached = new Dictionary<Vector2Int, Node>();
+    Node startNode;
+    Node destinationNode;
+    Node currentSearchNode;
+    Vector2Int[] directions = { Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down };
+    GridManager gridManager;
+    Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
+
+    void Awake()
+    {
+        gridManager = FindObjectOfType<GridManager>();
+        if(gridManager != null)
+        {
+            grid = gridManager.Grid;
+        }
+
+        startNode = new Node(startCoordinates, true);
+        destinationNode = new Node(destinationCoordinates, true);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BreadthFirstSearch();
+        //ExploreNeighbors();
+    }
+
+    void ExploreNeighbors()
+    {
+        List<Node> neighbors = new List<Node>();
+        foreach(Vector2Int direction in directions)
+        {
+            Vector2Int neighborCoordinates = currentSearchNode.coordinates + direction;
+            if(gridManager.Grid.ContainsKey(neighborCoordinates))
+            {
+                neighbors.Add(grid[neighborCoordinates]);
+
+            }
+            foreach(Node neighbor in neighbors)
+            {
+                if(!reached.ContainsKey(neighbor.coordinates) && neighbor.isWalkable)
+                {
+                    neighbor.connectedTo = currentSearchNode;
+                    reached.Add(neighbor.coordinates, neighbor);
+                    frontier.Enqueue(neighbor);
+                }
+            }
+        }
+    }
+
+    void BreadthFirstSearch()
+    {
+        bool isRunning = true;
+
+        frontier.Enqueue(startNode);
+        reached.Add(startCoordinates, startNode);
+
+        while(frontier.Count > 0 && isRunning)
+        {
+            currentSearchNode = frontier.Dequeue();
+            currentSearchNode.isExplored = true;
+            ExploreNeighbors();
+            if(currentSearchNode.coordinates == destinationCoordinates)
+            {
+                isRunning = false;
+            }
+        }
+    }
+
+}
--- Tower Defense/Assets/Scripts/NewCoordinateLabeler.cs
+++ Tower Defense/Assets/Scripts/NewCoordinateLabeler.cs
@@ -7,20 +7,22 @@ using TMPro;
 [ExecuteAlways]
 [RequireComponent(typeof(TextMeshPro))]
 
-public class CoordinateLabeler : MonoBehaviour
+public class NewCoordinateLabeler : MonoBehaviour
 {
     [SerializeField] Color defaultColor = Color.white;
     [SerializeField] Color blockedColor = Color.gray;
+    [SerializeField] Color exploredColor = Color.yellow;
+    [SerializeField] Color pathColor = new Color(1f, 0.5f, 0f);
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
 
-    Waypoint waypoint;
-
+    GridManager gridManager;
 
     void Awake()
     {
+        gridManager = FindObjectOfType<GridManager>();
         label = GetComponent<TextMeshPro>();
-        waypoint = GetComponentInParent<Waypoint>();
+        
         label.enabled = false;
         DisplayCoordinates();
        
@@ -42,6 +44,7 @@ public class CoordinateLabeler : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.C))
         {
+            Debug.Log("C key pressed");
             label.enabled = !label.enabled;
         }
     }
@@ -49,13 +52,26 @@ public class CoordinateLabeler : MonoBehaviour
     //sets label color based on if waypoint is placeable
     void SetLabelColor()
     {
-        if (waypoint.IsPlaceable)
+        if (gridManager == null) { return; }
+
+        Node node = gridManager.GetNode(coordinates);
+
+        if (node == null) { return; }
+        if(!node.isWalkable)
         {
-            label.color = defaultColor;
+            label.color = blockedColor;
+        }
+        else if (node.isPath)
+        {
+            label.color = pathColor;
+        }
+        else if (node.isExplored)
+        {
+            label.color = exploredColor;
         }
         else
         {
-            label.color = blockedColor;
+            label.color = defaultColor;
         }
     }
 
